1b0312052f71b869955a1d8c22ccf50da133280a
lzx <1400415006@qq.com>
2022-01-14 19:38:19 +0800

    add

---
+++ src/components/moduleDemo/math.js
@@ -0,0 +1,9 @@
+ import print from './print.js'
+
+export function add(a,b){
+    print(a+b)
+}
+
+export function chengfa(a,b){
+    print(a*b)
+}
---
+++ src/components/moduleDemo/test.js
@@ -0,0 +1,3 @@
+import { add, chengfa } from './math.js'
+        add(100,200)
+        chengfa(20,30)
